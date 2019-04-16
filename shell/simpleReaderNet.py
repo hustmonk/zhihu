87100fdf2baaaabde055369a4fe91634b6975a17
liujm <liujm@fenbi.com>
2019-04-16 12:08:38 +0800

    MOD:net

--- shell/simpleReaderNet.py
+++ shell/simpleReaderNet.py
@@ -13,14 +13,18 @@ class ReaderNet(nn.Module):
         self.args = args
 
         #first layer
-        self.scorer = layers.ScoreLayer(args.embedding_dim)
+        self.linear = nn.Linear(args.embedding_dim, 250)
 
     def forward(self, inputs):
-        passage, passage_mask, answer1, answer1_mask, answer2, answer2_mask = inputs
+        passage, passage_pooled, passage_mask, answer1, answer1_pooled, answer1_mask, answer2, answer2_pooled, answer2_mask = inputs
+
+        passage_pooled = self.linear(passage_pooled)
+        answer1_pooled = self.linear(answer1_pooled).unsqueeze(1)
+        answer2_pooled = self.linear(answer2_pooled).unsqueeze(1)
 
         #first layer
-        score1 = self.scorer(passage, passage_mask, answer1, answer1_mask)
-        score2 = self.scorer(passage, passage_mask, answer2, answer2_mask)
+        score1 = answer1_pooled.bmm(passage_pooled.unsqueeze(2)).squeeze(2)
+        score2 = answer2_pooled.bmm(passage_pooled.unsqueeze(2)).squeeze(2)
 
         answer = torch.cat([score1, score2], 1)
         answer = F.log_softmax(answer, dim=-1)
